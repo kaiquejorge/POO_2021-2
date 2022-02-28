1ed6796efaed0164df82fa9d3b4f617068d527b6
kaiquejorge <kaiquejor@gmail.com>
2022-02-28 01:08:22 -0300

    up

--- 07_tamagotchi/tamagotchi.ts
+++ 07_tamagotchi/tamagotchi.ts
@@ -2,8 +2,9 @@ const readline = require('readline-sync');
 let input = (): string => readline.question(); // p ler 
 let write = (x : any) => process.stdout.write("" + x); // p n quebrar a linha
 
+
 class Pet {
-  private nome: string = "";
+  private name: string = "";
   private alive: boolean;
   private age: number;
   private diamonds: number;
@@ -14,8 +15,7 @@ class Pet {
   private hungry: number = 0;
   private hungryMax: number;
 
-	constructor(nome: string, energy: number, hungry: number, clean: number){
-	  this.setNome(nome)
+	constructor(energy: number, hungry: number, clean: number){
     this.setEnergy(energy)
     this.setHungry(hungry)
     this.setClean(clean)
@@ -28,15 +28,15 @@ class Pet {
     this.cleanMax = clean;
 	}
 
-  public setNome(nome: string) {
-      if (nome.length == 0) {
-          this.nome = "bichinho";
+  public setNome(name: string) {
+      if (name.length == 0) {
+          this.name = "bichinho";
       } else {
-          this.nome = nome;
+          this.name = name;
       }
   }
   public getNome(): string {
-      return this.nome;
+      return this.name;
   }
   public getAge(): number{
       return this.age;
@@ -98,11 +98,12 @@ class Pet {
     }
   }
 
-  brincar(): void {
+  public play(): void {
     if(!this.isAlive) {
       console.log("pet morto não brinca\n");
       
-    } else if (this.isAlive){
+    } else if (this.alive){
+      this.alive = true;
       console.log("playing\n");
       
       this.setEnergy(this.energy - 2)
@@ -113,33 +114,43 @@ class Pet {
       this.diamonds++
     }     
   }
-  comer(): void {
-      if(!this.isAlive) {
-          console.log("pet morto não come\n");
-          return;
-      }else if (this.isAlive){
-        this.setEnergy(this.energy -1)
-        this.setHungry(this.hungry +4)
-        this.setClean(this.clean -2)
+  public eat(): void {
+    if(!this.isAlive) {
+        console.log("pet morto não come\n");
+        return;
+    }else if (this.alive){
+      this.setEnergy(this.energy -1)
+      this.setHungry(this.hungry +4)
+      this.setClean(this.clean -2)
 
-        this.age++ // e ai ele cresce
+      this.age++ // e ai ele cresce
+    }  
   }
-  shower(): void {
-       if(!this.isAlive) {
-          console.log("pet morto não toma banho\n");
-          return;
-      }else if (this.isAlive){
-        this.setEnergy(this.energy -1)
-        this.setHungry(this.hungry +4)
-        this.setClean(this.clean -2)
 
-        this.age++ // e ai ele cresce
-}
+  public shower(): void {
+    if(!this.isAlive) {
+      console.log("pet morto não toma banho\n");
+        return;
+    }else if (this.alive){
+    this.setEnergy(this.getEnergy() -1);
+    this.setHungry(this.getHungry() +4);
+    this.setClean(this.getCleanMax());
+
+    this.age +=1; // e ai ele cresce
+    }
+  }
+
   public sleep(): void {
-      
-  }
-  public energy(): void {
-      
+    if(!this.isAlive){
+      console.log("pet morto não dorme\n")
+    }else if (this.energy <= this.energyMax){
+      console.log("pet está memindo\n")
+    this.age += 1;
+    this.setEnergy(this.energyMax);
+    this.setHungry(this.hungry - 1);
+    }else{
+      console.log("o pet não está com sono")
+    }
   }
   
   public isAlive():boolean {
@@ -152,11 +163,27 @@ class Pet {
   }
 
   toString():string{
-    return `E:${this.energy}/${this.energyMax} S:${this.hungry}/${this.hungryMax} L:${this.clean}/${this.cleanMax} D:${this.diamonds} I:${this.age}`
+    return `Energia:${this.energy}/${this.energyMax} Saciedade:${this.hungry}/${this.hungryMax} Limpeza:${this.clean}/${this.cleanMax} Diamantes:${this.diamonds} Idade:${this.age}`
   }
 }
 
 class IO {
+      create_pet(): Pet {
+        write("qual o nome do seu pet?")
+        let name = +input()
+        
+        write("insira a energia máxima")
+        let energyMax = +input()
+    
+        write("insira a saciedade máxima")
+        let hungryMax = +input()
+    
+        write("insira o nível máximo de limpeza do seu pet")
+        let cleanMax = +input()
+    
+        let pet: Pet = new Pet(energyMax, hungryMax, cleanMax)
+        return pet
+    }  
     mostrar_help() {
         write("Comandos:\n");
         write("  init <nome> <energyMax> <cleanMax>: cria um novo pet\n");
@@ -168,7 +195,7 @@ class IO {
     }
 
     shell() {
-        let pet: Pet = create_pet();
+        let pet: Pet = this.create_pet();
         this.mostrar_help();
         while (true) {
             write("$ ");
@@ -179,7 +206,7 @@ class IO {
             } else if (words[0] == "help") {
                 this.mostrar_help();
             } else if (words[0] == "show") {
-                write("" + pet "\n");
+                write("" + pet );
             } else if (words[0] == "eat") {
                 pet.eat();
             } else if (words[0] == "play") {
@@ -189,7 +216,7 @@ class IO {
             } else if (words [0] == "sleep") {
                 pet.sleep();
             } else if (words[0] == "init") {
-                pet = new Pet(+words[1], +words[2], +words[3], +words[4]);
+                pet = new Pet(+words[1], +words[2], +words[3]);
             } else {
                 console.log("Comando inválido");
             }
